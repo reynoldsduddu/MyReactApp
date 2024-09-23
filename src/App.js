7a3bbb472e15e98e49988a75abffe7c13feac8c5
REYNOLDS DUDDU <61578612+reynoldsduddu@users.noreply.github.com>
2024-09-23 14:40:27 +0000

    Functional React App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
     </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces of advice
+    </p>
   );
 }
